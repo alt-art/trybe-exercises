d308b38f2f34458155e1601e80bc9249cf2a941d
alt-art <pedromendescraft@gmail.com>
2021-10-19 23:51:26 -0300

    Add exercise 7.1

---
+++ fundamentals/block 7/day 1/index.js
@@ -0,0 +1,18 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(`Os números ${oddsAndEvens.join(', ')} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
