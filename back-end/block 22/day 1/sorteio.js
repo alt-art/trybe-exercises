5fccacba859cd427e0cfa3e0a49dd67accf80417
Pedro Mendes <pedromendescraft@gmail.com>
2022-03-12 05:34:28 -0300

    fix: Play again need function

--- back-end/block 22/day 1/sorteio.js
+++ back-end/block 22/day 1/sorteio.js
@@ -1,22 +1,26 @@
 const read = require('readline-sync');
 
-const aswer = read.questionInt('Qual é o numero a ser sorteado? ');
+function playGame() {
+  const aswer = read.questionInt('Qual é o numero a ser sorteado? ');
 
-const randomNumber = Math.floor(Math.random() * 10);
+  const randomNumber = Math.floor(Math.random() * 10);
 
-if (aswer === randomNumber) {
-  console.log('Parabéns, numero correto!');
-} else {
-  console.log(`Opa não foi dessa vez, o numero sorteado foi ${randomNumber}`);
+  if (aswer === randomNumber) {
+    console.log('Parabéns, numero correto!');
+  } else {
+    console.log(`Opa não foi dessa vez, o numero sorteado foi ${randomNumber}`);
+  }
+
+  const playAgain = read.question('Deseja jogar novamente? (y/n) ');
+
+  if (
+    playAgain.toLowerCase() === 's' ||
+    playAgain.toLowerCase() === 'sim' ||
+    playAgain.toLowerCase() === 'y' ||
+    playAgain.toLowerCase() === 'yes'
+  ) {
+    playGame();
+  }
 }
 
-const playAgain = read.question('Deseja jogar novamente? (y/n) ');
-
-if (
-  playAgain.toLowerCase() === 's' ||
-  playAgain.toLowerCase() === 'sim' ||
-  playAgain.toLowerCase() === 'y' ||
-  playAgain.toLowerCase() === 'yes'
-) {
-  playGame();
-}
+playGame();
